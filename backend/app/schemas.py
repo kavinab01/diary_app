d20d3c14ec7a2ed0c64513a31b1c4666158b86d1
kavinab01 <kavinab01@gmail.com>
2025-08-20 15:26:02 +0530

    Create schemas.py

---
+++ backend/app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from datetime import date
+
+class NoteBase(BaseModel):
+    note_date: date
+
+class NoteCreate(NoteBase):
+    note_text: str
+
+class Note(NoteBase):
+    id: int
+    note_text: str
+    created_at: str | None = None
+
+    class Config:
+        from_attributes = True
